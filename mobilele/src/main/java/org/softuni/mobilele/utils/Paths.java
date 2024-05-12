fbdac2e80e0569d5d4c87775abc897a3fbf7ea9b
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2024-05-12 22:18:16 +0300

    Adding the paths of the static inputs

---
+++ mobilele/src/main/java/org/softuni/mobilele/utils/Paths.java
@@ -0,0 +1,11 @@
+package org.softuni.mobilele.utils;
+
+public enum Paths {
+    ;
+
+    public static final String PATH_BRANDS = "src/main/resources/inputs/brands.json";
+    public static final String PATH_MODELS = "src/main/resources/inputs/models.json";
+    public static final String PATH_OFFERS = "src/main/resources/inputs/offers.json";
+    public static final String PATH_USER_ROLE = "src/main/resources/inputs/user_role.json";
+    public static final String PATH_USERS= "src/main/resources/inputs/users.json";
+}
