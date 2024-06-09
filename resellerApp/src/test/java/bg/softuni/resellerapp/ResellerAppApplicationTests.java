c434ae5b417ce10e6367347b5f5f17731a446679
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2024-06-09 16:58:10 +0300

    Importing skeleton of the first exam preparation.6 hours in total for completion.

---
+++ resellerApp/src/test/java/bg/softuni/resellerapp/ResellerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.resellerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResellerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
