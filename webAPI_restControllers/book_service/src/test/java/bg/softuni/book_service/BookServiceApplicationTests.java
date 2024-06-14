5e0d74bf1603ff24893110e375c71ec5ff1b3c5c
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2024-06-14 18:58:00 +0300

    Adding the skeleton of the API and Rest Controllers exercise

---
+++ webAPI_restControllers/book_service/src/test/java/bg/softuni/book_service/BookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.book_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
