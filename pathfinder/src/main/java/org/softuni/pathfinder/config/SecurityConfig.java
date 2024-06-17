fb4cd8da69b6c73bbcceab62cb50f160c9c7d558
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2024-06-17 23:28:22 +0300

    Adding a TODO for tomorrow in pathfinder

---
+++ pathfinder/src/main/java/org/softuni/pathfinder/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package org.softuni.pathfinder.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+    // TODO : Implement Spring Security and get rid of CurrentUser class and logic
+}
